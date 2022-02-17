64a1a6859d8bf8024773b90bb3af3e63670c6303
Brian Aston <astonb.dev@gmail.com>
2022-02-16 16:15:18 -0800

    Finished get specific and patch item

--- routing/items.js
+++ routing/items.js
@@ -3,16 +3,38 @@ const db = require("./fakeDb");
 
 const router = express.Router();
 
+/** get all items */
 router.get("/", function (req, res) {
     return res.json(db.items);
 });
 
+/** add item */
 router.post("/", function (req,res){
-    let newItem = { name: req.params.name, price: req.params.price};
-    db.items.push(newItem);
+    const newItem = { name: req.params.name, price: req.params.price};
+    //db.items.push(newItem);
+    db.items[newItem.name] = newItem;
     return res.json({ added: newItem});
 });
 
+/** get :name item */
+router.get("/:name", function (req, res){
+    const fetchedItem = db.items[req.params.name];
+
+    return res.json(fetchedItem);
+});
+
+/** update item in shopping list */
+router.patch("/:name", function(req, res){
+    const fetchedItem = db.items[req.params.name];
+
+    for(key in req.params){
+        fetchedItem[key] = req.params[key];
+    }
+
+    return res.json({updated: fetchedItem});
+});
+
+/** delete item from shopping list */
 
 module.exports = router;
 
